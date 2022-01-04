ee094f911dd72d1f0520370f8981fdc65575cf12
glimmerli <2521457775@qq.com>
2022-01-04 23:41:17 +0800

    fist add javatest project

---
+++ src/testdome01.java
@@ -0,0 +1,15 @@
+public class testdome01 {
+
+    public  static void main(String[] args ){
+        System.out.print("你好，Java");
+    }
+}
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+}
+
+
+
+
