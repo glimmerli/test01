6b0b8af19803848d5320c773dd959e4ebfe33587
glimmerli <2521457775@qq.com>
2022-01-06 22:30:42 +0800

    带返回值的最大值方法 addto 定义格式

--- src/MethodDome02.java
+++ src/MethodDome02.java
@@ -2,6 +2,12 @@ public class MethodDome02 {
     public static void main(String[] args) {
         isEvenNumber(5,6);
     }
+    // 定义格式
+    /*
+        public static 数据类型 方法名（）{
+            return 数据；
+            }
+     */
     public static int isEvenNumber(int a,int b){
         int max;
         System.out.println((max=a>b?a:b));
