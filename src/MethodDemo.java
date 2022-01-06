9e8c8740171bd09ca4763fa894235dca3a832b57
glimmerli <2521457775@qq.com>
2022-01-06 22:09:32 +0800

    isEvenNumber（int number） 带参数的方法

--- src/MethodDemo.java
+++ src/MethodDemo.java
@@ -1,6 +1,6 @@
 public class MethodDemo {
     public static void main(String[] args) {
-        isEvenNumber();
+        isEvenNumber(9);
     }
     //方法格式
 /*    public static void 方法名(){
@@ -8,13 +8,13 @@ public class MethodDemo {
    }
    */
     //方法 判断数据是否为偶数
-    public static void isEvenNumber (){
-        int number =10;
+    public static void isEvenNumber (int number){
+
         //判断
         if(number%2==0){
             System.out.println("偶数");
         }else{
-            System.out.println("基数");
+            System.out.println("奇数");
         }
     }
 }
